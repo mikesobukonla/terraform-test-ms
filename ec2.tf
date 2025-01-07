77aca49a9f2eee0cde062f6833e4086886276dbe
Michael Sobukonla <mike.sobukonla@gmail.com>
2025-01-07 00:32:44 +0000

    updated project config

--- ec2.tf
+++ ec2.tf
@@ -1,16 +1,25 @@
-module "ec2_instance" {
-  source = "terraform-aws-modules/ec2-instance/aws"
-
-  name = "single-instance"
-
-  instance_type          = "t2.micro"
-  key_name               = "user1"
-  monitoring             = true
-  vpc_security_group_ids = ["sg-12345678"]
-  subnet_id              = "subnet-eddcdzz4"
-
+resource "aws_instance" "web2" {
+  ami           = "ami-0453ec754f44f9a4a"
+  instance_type = "t2.micro"
   tags = {
-    Terraform   = "true"
-    Environment = "dev"
+
+    Name = "HelloWorld"
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+ 
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  assume_role {
+    role_arn = "arn:aws:iam::851725374695:role/ec2Admin"
   }
 }
